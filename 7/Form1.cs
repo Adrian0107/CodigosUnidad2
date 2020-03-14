cd786a19075a576b0632f381e846d0b5deba7050
Adrian0107 <61296862+Adrian0107@users.noreply.github.com>
2020-03-13 20:15:12 -0600

    Add files via upload

---
+++ 7/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Beisbol
+{
+    public partial class Form1 : Form
+    {
+        Pitcher pitcher = new Pitcher();
+        Posicion posicion = new Posicion();
+        Designado designado = new Designado();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                pitcher.NumUniforme = int.Parse(textBox1.Text);
+                pitcher.Nombre = textBox2.Text;
+                pitcher.Ponches= int.Parse(textBox3.Text);
+                pitcher.Errores = int.Parse(textBox5.Text);
+          
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox5.Clear();
+            }
+            if (radioButton2.Checked == true)
+            {
+                posicion.NumUniforme = int.Parse(textBox1.Text);
+                posicion.Nombre = textBox2.Text;
+                posicion.Hits = int.Parse(textBox4.Text);
+                posicion.Errores = int.Parse(textBox5.Text);
+               
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox4.Clear();
+                textBox5.Clear();
+            }
+            if (radioButton3.Checked == true)
+            {
+                designado.NumUniforme = int.Parse(textBox1.Text);
+                designado.Nombre = textBox2.Text;
+                designado.Hits = int.Parse(textBox4.Text);
+               
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox4.Clear();
+               
+            }
+            
+        }
+    
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox4.Enabled = false;
+            textBox3.Enabled = true;
+            textBox5.Enabled = true;
+
+              
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox4.Enabled = true;
+            textBox3.Enabled = false;
+            textBox5.Enabled = true;
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox4.Enabled = true;
+            textBox3.Enabled = false;
+            textBox5.Enabled = false;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                MessageBox.Show(pitcher.ToString());
+            }
+            if (radioButton2.Checked == true)
+            {
+                MessageBox.Show(posicion.ToString());
+            }
+            if (radioButton3.Checked == true)
+            {
+                MessageBox.Show(designado.ToString());
+            }
+         
+        }
+    }
+}
---
+++ 7/Jugadores.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beisbol
+{
+    class Jugadores
+    {
+        private int numUniforme;
+
+        public int NumUniforme
+        {
+            get { return numUniforme; }
+            set { numUniforme = value; }
+        }
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+
+    }
+    class Pitcher : Jugadores
+    {
+        private int ponches;
+
+        public int Ponches
+        {
+            get { return ponches; }
+            set { ponches = value; }
+        }
+        private int errores;
+
+        public int Errores
+        {
+            get { return errores; }
+            set { errores = value; }
+        }
+        public override string ToString()
+        {
+            return ("Datos del Pitcher: \n \nNumero de jugador: " + this.NumUniforme + " \nNombre: " + this.Nombre + "\nPonches: " + this.Ponches + "\nErrores: " + this.Errores );
+        }
+
+
+    }
+    class Posicion : Jugadores
+    {
+        private int hits;
+
+        public int Hits
+        {
+            get { return hits; }
+            set { hits = value; }
+        }
+        private int errores;
+
+        public int Errores
+        {
+            get { return errores; }
+            set { errores = value; }
+        }
+        public override string ToString()
+        {
+           return ("Datos del jugador de posicion: \n \nNumero de jugador: " + this.NumUniforme + " \nNombre: " + this.Nombre + "\nHits: " + this.Hits + "\nErrores: " + this.Errores);
+        }
+
+
+
+    }
+    class Designado : Jugadores
+    {
+        private int hits;
+
+        public int Hits
+        {
+            get { return hits; }
+            set { hits = value; }
+        }
+        public override string ToString()
+        {
+            return ("Datos del jugador designado: \n \nNumero de jugador: " + this.NumUniforme + " \nNombre: " + this.Nombre+ "\nHits: " + this.Hits );
+        }
+
+    }
+  
+}
